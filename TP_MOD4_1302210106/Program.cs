6102f0647fe03321f7fd58b105530726609bf9f7
Law1911 <epensusanto@gmail.com>
2023-03-12 22:04:01 +0700

    menambahkan implementasi dengan table-driven

--- TP_MOD4_1302210106/Program.cs
+++ TP_MOD4_1302210106/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace TP_MOD4_KPL
+{
+    class Mainprogram
+    {
+        public static void Main(string[] args)
+        {
+            KodePos table_kode_pos = new KodePos();
+            Console.Write("Masukan Kota yang ingin dilihat kode pos : ");
+            String kota = Console.ReadLine();
+
+            table_kode_pos.getkodepos(kota);
+
+            Console.WriteLine("");
+        }
+    }
+    class KodePos
+    {
+        Dictionary<string, int> dic_Kode_daerah = new Dictionary<string, int>(){
+            {"Batununggal", 40266},
+            {"Kujangsari", 40287},
+            {"Mengger", 40267},
+            {"Wates", 40256},
+            {"Cijaura", 40287},
+            {"Jatisari", 40286},
+            {"Margasari", 40286},
+            {"Sekejati", 40286},
+            {"Kebonwaru", 40272},
+            {"Maleer", 40274},
+            {"Samoja", 40273},
+        };
+
+        public void getkodepos(string kode)
+        {
+            if (dic_Kode_daerah.ContainsKey(kode))
+            {
+                Console.WriteLine(kode + " : " + dic_Kode_daerah[kode]);
+            }
+            else
+            {
+                Console.WriteLine(kode + "Kode tidak terdaftar");
+            }
+        }
+    }
+}
+    
